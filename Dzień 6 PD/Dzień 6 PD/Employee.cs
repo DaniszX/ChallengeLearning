f909c09447eac235995d8c751ba59d8a965770d3
Danisz X <daniszek50@gmail.com>
2023-02-01 14:14:13 +0100

    Day 6 Learning

---
+++ Dzień 6 PD/Dzień 6 PD/Employee.cs
@@ -0,0 +1,33 @@
+﻿namespace Dzień_6_PD
+{
+    internal class Employee
+    {
+        public static string Grades = "Highest Grade";
+        private List<int> grade = new List<int>();
+
+        public Employee(int grade, string Name, String Surname, string Age)
+        {
+            this.Name = Name;
+            this.Surname = Surname;
+            this.Age = Age;
+            Grades = null;
+        }
+
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public string Age { get; private set; }
+
+        public int Grade
+        {
+            get
+            {
+                return this.grade.Sum();
+            }
+        }
+
+        public void AddScore(int grade)
+        {
+            this.grade.Add(grade);
+        }
+    }
+}
---
+++ Dzień 6/Dzień 6/Program.cs
@@ -0,0 +1,29 @@
+﻿using Dzień_6;
+
+User user1 = new User("Anakin", "Vader");
+User user2 = new User("Kenobi", "ObiWan");
+User user3 = new User("Padme", "Amidala");
+User user4 = new User("JarJar", "Secret_Sith");
+
+user1.AddScore(5);
+user1.AddScore(2);
+var result = user1.Result;
+Console.WriteLine(result);
+var name = User.GameName;
+var pie = Math.PI;
+
+List<User> users = new List<User>()
+{
+    user1, user2, user3
+};
+
+int maxResult = -1;
+User employeeWithHighestGrade = null;
+
+foreach (var user in users)
+{
+    if (user.Result > maxResult)
+    {
+        employeeWithHighestGrade = user;
+    }
+}
