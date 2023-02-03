c22aef445f977394f60007f6dbec1d04d5d12930
Danisz X <daniszek50@gmail.com>
2023-02-03 15:12:20 +0100

    Day 8 Learning

---
+++ Dzień 7/Dzień 7.Tests/TypeTests_Day8.cs
@@ -0,0 +1,43 @@
+﻿namespace Dzień_7.Tests
+{
+    public class TypeTests_Day8
+    {
+        [Test]
+        public void GetGradeShouldReturnSameValue()
+        {
+            float Grade1 = 74564567;
+            float Grade2 = 74564567;
+            Assert.AreEqual(Grade1, Grade2);
+        }
+
+        [Test]
+        public void GetEmployeeShouldReturnDifferentObjects()
+        {
+            var emp1 = GetEmployee(0, "Anakin", "Skywalker", "22");
+            var emp2 = GetEmployee(0, "Obi-Wan", "Kenobi", "38");
+            Assert.AreNotEqual(emp1.Name, emp2.Name);
+        }
+
+        [Test]
+        public void GetAgeShouldReturnDifferentValue()
+        {
+            int Age1 = 22;
+            int Age2 = 38;
+            Assert.AreNotEqual(Age1, Age2);
+        }
+
+        [Test]
+        public void GetSurnameShouldReturnDifferentObjects()
+        {
+            string surname1 = "Skywalker";
+            string surname2 = "Kenobi";
+            Assert.AreNotEqual(surname1, surname2);
+        }
+
+
+        private Employee GetEmployee(int grade, string name, string surname, string age)
+        {
+            return new Employee(grade, name, surname, age);
+        }
+    }
+}
